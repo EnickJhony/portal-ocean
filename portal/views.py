80bd8c9165bdf3f6dee16dbcec45a93b906ad8e7
Enick Jhony <enick.jhony@gmail.com>
2022-10-06 21:30:05 -0300

    finalizando a aula

--- portal/views.py
+++ portal/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import redirect, render
 
+
+from django.contrib import messages
 # Create your views here.
 
 
--- projeto01/urls.py
+++ projeto01/urls.py
@@ -7,6 +7,6 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
-    # path('admin/', admin.site.urls),
+    path('admin/', admin.site.urls),
     path('', views.home, name='home'),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
